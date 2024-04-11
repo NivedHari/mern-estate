838c835f0473d8f7a564929c9d3559b25abc6ca6
NivedHari <nivedhari44@gmail.com>
2024-04-12 01:47:04 +0530

    ready for deployment

--- api/index.js
+++ api/index.js
@@ -2,11 +2,12 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path";
 dotenv.config();
 
 import userRouter from "./routes/user.router.js";
 import authRouter from "./routes/auth.route.js";
-import listingRouter from './routes/listing.route.js'
+import listingRouter from "./routes/listing.route.js";
 
 mongoose
   .connect(process.env.MONGO)
@@ -16,6 +17,8 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+const __dirname = path.resolve();
 
 const app = express();
 
@@ -29,8 +32,13 @@ app.listen(3000, () => {
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
-app.use("/api/listing", listingRouter)
+app.use("/api/listing", listingRouter);
 
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
